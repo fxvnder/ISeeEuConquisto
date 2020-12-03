7de611f9d804fd94439f850da34ad5a15e02010a
EdEquinox <47253077+EdEquinox@users.noreply.github.com>
2020-12-03 15:25:29 +0000

    .

--- ISeeEuConquisto/territorios.cpp
+++ ISeeEuConquisto/territorios.cpp
@@ -1,9 +1,25 @@
 #include "territorios.h"
+#include <vector>
+#include <memory>
+#include <string>
 
-// PARA ME LEMBRAR
-// FAZER FUNCAO GERARNOVOTERRENO(COORDENADA X, COORDENADA Y)
-// ONDE O JOGADOR ESTA NA POSICAO - 500, 000 (EXEMPLO)O TERRENO É GERADO NA DIREÇAO QUE O JOGADOR VAI, POR EXEMPLO
-// ESQUERDA OESTE GERARNOVOTERRENO(PLAYERX - 500, PLAYERY)
-// DIREITA ESTE GERARNOVOTERRENO(PLAYERX + 500, PLAYERY)
-// CIMA NORTE GERARNOVOTERRENO(PLAYERX, PLAYERY + 500)
-// BAIXO SUL GERARNOVOTERRENO(PLAYERX, PLAYERY - 500)
+using namespace std;
+
+class territorios {
+
+    int tamanho;
+
+public:
+    territorios(int tamanho) : tamanho(tamanho){
+    cout << "Territorio criado..." << endl;
+    };
+
+    ~territorios(){
+        cout << "Territorio apagado..." << endl;
+    }
+
+    string toString(){
+        return to_string(tamanho);
+    }
+};
+
